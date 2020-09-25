8549ca64948f76c535f59b23137005bfc429b41a
Leonard Greulich <l_greu02@uni-muenster.de>
2020-09-25 15:34:36 +0200

    Real-time evaluating range checks

--- js/helper/validationhelper.js
+++ js/helper/validationhelper.js
@@ -34,7 +34,32 @@ export function process(itemsWithRangeChecks) {
     }
 
     // Second, handle range checks
-    console.log(itemsWithRangeChecks);
+    for (let item of itemsWithRangeChecks) {
+        let input = $(`#clinicaldata-content [preview-oid='${item.itemOID}']`);
+        input.addEventListener("focusout", event => {
+            for (let rangeCheck of item.rangeChecks) {
+                switch (rangeCheck.comparator) {
+                    case "LT":
+                        if (rangeCheckValue(input.value) >= rangeCheck.checkValue) showRangeCheckWarning(event.target);
+                        break;
+                    case "LE":
+                        if (rangeCheckValue(input.value) > rangeCheck.checkValue) showRangeCheckWarning(event.target);
+                        break;
+                    case "GT":
+                        if (rangeCheckValue(input.value) <= rangeCheck.checkValue) showRangeCheckWarning(event.target);
+                        break;
+                    case "GE":
+                        if (rangeCheckValue(input.value) < rangeCheck.checkValue) showRangeCheckWarning(event.target);
+                        break;
+                    case "EQ":
+                        if (input.value && isDecimal(input.value) && rangeCheckValue(input.value) != rangeCheck.checkValue) showRangeCheckWarning(event.target);
+                        break;
+                    case "NE":
+                        if (rangeCheckValue(input.value) == rangeCheck.checkValue) showRangeCheckWarning(event.target);
+                }
+            }
+        });
+    }
 }
 
 function isInt(value) {
@@ -47,4 +72,14 @@ function isDecimal(value) {
 
 function isDate(value) {
     return !isNaN(Date.parse(value));
+}
+
+function rangeCheckValue(value) {
+    let float = parseFloat(value);
+    return value == float ? float : undefined;
+}
+
+function showRangeCheckWarning(input) {
+    ioHelper.showWarning("Problem", "RangeCheck problem.");
+    input.focus();
 }
