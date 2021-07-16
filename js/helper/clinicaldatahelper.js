78f76a22e74ecaac69f0de97fcb6164ee400a11d
Leonard Greulich <l_greu02@uni-muenster.de>
2021-07-16 17:16:08 +0200

    Improved check whether data was edited

--- js/helper/clinicaldatahelper.js
+++ js/helper/clinicaldatahelper.js
@@ -309,7 +309,7 @@ function getFormItemDataList(formData) {
 
 export function dataHasChanged(formItemDataList, studyEventOID, formOID) {
     console.log("Check if data has changed ...");
-    const sortedNewData = formItemDataList.sort((a, b) => a.itemOID < b.itemOID ? 1 : (a.itemOID > b.itemOID ? -1 : 0));
+    const sortedNewData = Array.from(formItemDataList).sort((a, b) => a.itemOID < b.itemOID ? 1 : (a.itemOID > b.itemOID ? -1 : 0));
     const sortedOldData = getSubjectFormData(studyEventOID, formOID).sort((a, b) => a.itemOID < b.itemOID ? 1 : (a.itemOID > b.itemOID ? -1 : 0));
     return JSON.stringify(sortedNewData) != JSON.stringify(sortedOldData);
 }
