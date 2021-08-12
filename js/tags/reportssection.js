d2ed721337828d4fef164aafd49d1f68a0f1d094
Leonard Greulich <l_greu02@uni-muenster.de>
2021-08-12 12:01:27 +0200

    Further reporting preparations

--- js/tags/reportssection.js
+++ js/tags/reportssection.js
@@ -5,7 +5,11 @@ class ReportsSection extends HTMLElement {
                 <div class="columns is-desktop">
                     <div class="column is-one-fifth-desktop">
                         <nav class="panel is-link">
-                            <p class="panel-heading has-text-centered" i18n="reports"></p
+                            <p class="panel-heading has-text-centered" i18n="reports"></p>
+                            <div class="tree-panel-blocks" id="reports-panel-blocks"></div>
+                            <div class="panel-block has-light-border-top">
+                                <button class="button is-link is-light is-fullwidth" id="reports-add-button" onclick="addReport()" i18n="add"></button>
+                            </div>
                         </nav>
                     </div>
                     <div class="column">
