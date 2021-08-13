9595adaa30cdf912aec72547743993893cb6328c
Leonard Greulich <l_greu02@uni-muenster.de>
2021-08-13 10:05:20 +0200

    Restructured reports section

--- js/components/reportssection.js
+++ js/components/reportssection.js
@@ -3,21 +3,31 @@ class ReportsSection extends HTMLElement {
         this.innerHTML = `
             <section class="section is-hidden" id="reports-section">
                 <div class="columns is-desktop">
+                    <div class="column is-offset-one-fifth">
+                        <div class="container">
+                            <h1 class="title"></h1>
+                            <h2 class="subtitle"></h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="columns is-desktop">
                     <div class="column is-one-fifth-desktop">
-                        <aside class="menu mx-5">
-                            <p class="menu-label" i18n="reports"></p>
-                            <div class="menu-list" id="reports-list"></div>
-                            <p class="menu-label is-hidden" i18n="data-quality"></p>
-                            <div class="menu-list is-hidden"></div>
-                        </aside>
-                        <div class="buttons m-5">
-                            <button class="button is-link is-light is-small" id="add-report-button" i18n="add"></button>
+                        <div class="card">
+                            <div class="card-content">
+                                <aside class="menu">
+                                    <p class="menu-label" i18n="reports"></p>
+                                    <div class="menu-list" id="reports-list"></div>
+                                    <p class="menu-label is-hidden" i18n="data-quality"></p>
+                                    <div class="menu-list is-hidden"></div>
+                                </aside>
+                                <div class="buttons mt-5">
+                                    <button class="button is-link is-light is-small" id="add-report-button" i18n="add"></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="column">
                         <div class="container">
-                            <h1 class="title"></h1>
-                            <h2 class="subtitle"></h2>
                             <div id="widgets"></div>
                         </div>      
                     </div>
