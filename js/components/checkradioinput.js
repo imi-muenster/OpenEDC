493f4be2f1693015eb28ddd3bb0d66bf8f7d12ae
Leonard Greulich <l_greu02@uni-muenster.de>
2021-09-11 14:32:05 +0200

    Added custom checkbox web component

---
+++ js/components/checkradioinput.js
@@ -0,0 +1,49 @@
+class CustomCheckbox extends HTMLElement {
+    static get observedAttributes() {
+        return ["checked", "disabled"];
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.render();
+    }
+
+    attributeChangedCallback(name) {
+        if (this.input[name]) this.input[name] = false;
+        else this.input[name] = true;
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>${this.style}</style>
+            <label class="checkbox">
+                <input type="checkbox"></input>
+                <span><slot></slot></span>
+            </div>
+        `;
+    }
+
+    get style() {
+        return `
+            label {
+                display: block;
+                cursor: pointer;
+            }
+            input {
+                margin: 0 .25rem 0 0;
+                cursor: pointer;
+            }
+        `;
+    }
+
+    get input() {
+        return this.shadowRoot.querySelector("input");
+    }
+
+    get checked() {
+        return this.input.checked;
+    }
+}
+
+window.customElements.define("custom-checkbox", CustomCheckbox);
